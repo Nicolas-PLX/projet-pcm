209d6bd6e056e5ed4e28306b4820c084bfdc30e0
peneloux <nicolas.peneloux@free.fr>
2023-11-27 18:54:51 +0100

    Suite modif pour le navigator, correction bug et début menu pour sélection

--- app/src/main/java/fr/pcm/projet/projet_pcm/MainActivity.kt
+++ app/src/main/java/fr/pcm/projet/projet_pcm/MainActivity.kt
@@ -2,6 +2,8 @@ package fr.pcm.projet.projet_pcm
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toolbar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,8 +24,13 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -34,16 +41,22 @@ import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -81,7 +94,7 @@ fun menuDemarrage(){
         //content = { centreMenu() }, //Je l'ai commenté car sinon ça marchait pas
         bottomBar = { BottomBar(navController)}){ padding ->
         NavHost(navController = navController, startDestination = "home", modifier = Modifier.padding(padding)){
-            composable("home"){ centreMenu(padding)} //Mettre ici la fonction pour l'écran principale
+            composable("home"){ centreMenu(padding,navController)} //Mettre ici la fonction pour l'écran principale
             composable("jouer"){ GameScreen(padding)}
             composable("modifier"){ GestionDatabaseScreen(padding) }
         }
@@ -95,16 +108,16 @@ fun BottomBar(navController: NavHostController) = BottomNavigation{
     val currentRoute = navBackStackEntry?.destination?.route
     BottomNavigationItem(
         selected = currentRoute == "home",
-        onClick = { navController.navigate("home") },
-        icon = { })
+        onClick = { navController.navigate("home"){launchSingleTop = true} },
+        icon = {Icon(Icons.Default.Home,"home") })
     BottomNavigationItem(
         selected = currentRoute == "jouer",
-        onClick = { navController.navigate("jouer") },
-        icon = { /*TODO*/ })
+        onClick = { navController.navigate("jouer"){popUpTo("home")} },
+        icon = { Icon(Icons.Default.ArrowForward,"jouer") })
     BottomNavigationItem(
         selected = currentRoute == "modifier",
-        onClick = { navController.navigate("modifier") },
-        icon = { /*TODO*/ })
+        onClick = { navController.navigate("modifier"){popUpTo("home")} },
+        icon = { Icon(Icons.Default.Build, "modifier") })
 }
 
 
@@ -114,7 +127,6 @@ fun BottomBar(navController: NavHostController) = BottomNavigation{
 fun TopBarPrincipal() = // Dans l'idéal, ici mettre le nom du jeu ou je sais pas quoi, avec le bouton paramètre ?
     TopAppBar(title = {Text(text = "Bienvenue !", modifier = Modifier
         .fillMaxWidth()
-        .background(Color(248, 203, 219))
         /*.fillMaxHeight()*/,
         // TODO : faire l'alignement (je suis golmon) .align(CenterVertically),
         style = TextStyle(color = Color.Black, fontSize = 24.sp))},
@@ -127,12 +139,19 @@ fun TopBarPrincipal() = // Dans l'idéal, ici mettre le nom du jeu ou je sais pa
                     .background(Color.Transparent)
                     .clickable {/*Todo: action sur le click*/
                     })
-        }, modifier = Modifier.background(Color.Green) //Todo: couleur : ça ne marche pas
+        }, modifier = Modifier.background(Color.Green),
+        colors = TopAppBarDefaults.largeTopAppBarColors(
+            containerColor = Color(248,203,219),
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        )
+
     )
 
 
 @Composable
-fun centreMenu(padding : PaddingValues){
+fun centreMenu(padding : PaddingValues,navController: NavHostController){
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
     Column (
         modifier = Modifier
             .padding(vertical = 64.dp) //taille parfaite par rapport au TopAppBar : au dessus on dépasse, en dessous espace vide
@@ -140,11 +159,13 @@ fun centreMenu(padding : PaddingValues){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
         Row(){
-            Button(onClick = { /*TODO Choisir un thème*/ },
+            Button(onClick = { currentRoute == "jouer"
+                navController.navigate("jouer"){popUpTo("home")} },
             modifier = Modifier.padding(end = 8.dp)) {
                 Text(text = "Jouer")
             }
-            Button(onClick = { /*TODO modifier la BDD ?*/},
+            Button(onClick = { currentRoute == "modifier"
+                             navController.navigate("modifier"){popUpTo("home")}},
                 modifier = Modifier.padding(start = 8.dp)) {
                 Text(text = "Modifier")
             }
@@ -157,6 +178,27 @@ fun centreMenu(padding : PaddingValues){
 //Composable pour l'onglet "Jouer"
 @Composable
 fun GameScreen(padding : PaddingValues, model: GameModel = GameModel()){
+    val context = LocalContext.current
+    val themes = LocalContext.current.resources.getStringArray(R.array.theme_array)
+    var selected by remember {mutableStateOf("")}
+    Column(modifier = Modifier
+        .padding(vertical = 64.dp) //taille parfaite par rapport au TopAppBar : au dessus on dépasse, en dessous espace vide
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        /*
+        Spinner(
+            TODO : voir comment faire un bon spinner (les changements de package aident pas ...
+            context = context,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            value = selectedTheme,
+            onValueChange = { selectedTheme = it },
+            items = themeOptions
+        )*/
+        Text(text = "BONJOUR")
+    }
 
 }
 
